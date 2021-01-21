f875aa98c750d1216133daf4cc04c4b2d0bbddaa
Isaac Barreto <isaacssb123@gmail.com>
2021-01-21 18:03:54 -0300

    Complemento e treinamento

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("teste");
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function (field, index) {
+
+if (field.name == "gender") {
+
+  if (field.checked) {
+    console.log("SIM", field);
+
+  }
+
+} else {
+    console.log("NÃO");
+
+  }
+
+//console.log(field.id, field.name, field.value, field.checked, index);
+
+})
