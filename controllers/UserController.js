51dcd1df1dcdf6e60c7acd5f6219a5ba3a8d1279
Isaac Barreto <33076131+isaacssb@users.noreply.github.com>
2021-03-11 22:49:29 -0300

    alterações promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,38 +16,44 @@ class UserController {
 
       let values = this.getValues();
 
-      this.getPhoto((content)=>{
-
-        values.photo = content;
-
-        this.addLine(values);
-
-      });
+      this.getPhoto().then(
+        (content)=> {
+          values.photo = content;
+          this.addLine(values);
+        }, 
+        (e)=> {
+          console.error(e);
+        }
+      );
 
     });
 
   }
 
-  getPhoto(callback){
+  getPhoto(){
 
-    let fileReader = new FileReader();
+    return new Promise((resolve, reject)=> { 
+      let fileReader = new FileReader();
 
-    let elements = [...this.formEl.elements].filter(item=>{
-      if (item.name === 'photo') {
-        return item;
+      let elements = [...this.formEl.elements].filter(item=>{
+        if (item.name === 'photo') {
+          return item;
+        }
+      });
+  
+      let file = elements[0].files[0];
+  
+      fileReader.onload = ()=> {
+  
+        resolve(fileReader.result);
+  
+      };
+  
+      fileReader.onerror = ()=> {
+        reject(e);
       }
+      fileReader.readAsDataURL(file);
     });
-
-    let file = elements[0].files[0];
-
-    fileReader.onload = ()=> {
-
-      callback(fileReader.result);
-
-    };
-
-    fileReader.readAsDataURL(file);
-
   }
 
   getValues(){
