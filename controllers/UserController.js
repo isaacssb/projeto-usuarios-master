35a7e77d1fff7379f61e81a38ca2aef3e12d0f17
Isaac Barreto <33076131+isaacssb@users.noreply.github.com>
2021-03-14 15:51:20 -0300

    Aula 43

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -52,8 +52,15 @@ class UserController {
       fileReader.onerror = ()=> {
         reject(e);
       }
-      fileReader.readAsDataURL(file);
+      if (file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve('dist/img/boxed-bg.jpg');
+      }
+      
     });
+
+
   }
 
   getValues(){
@@ -68,6 +75,8 @@ class UserController {
           user[field.name] = field.value;
 
         }
+      } else if (field.name == "admin") {
+        user[field.name] = field.checked;
       } else {
           user[field.name] = field.value;
 
@@ -89,19 +98,23 @@ class UserController {
 
   addLine(dataUser) {
 
+    let tr = document.createElement('tr');
 
-    this.tableEl.innerHTML = `
+    tr.innerHTML = `
       <tr>
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
         <td>${dataUser.birth}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
-      </tr>`;
+      </tr>
+    `;
+
+    this.tableEl.appendChild(tr); 
   }
 
 }
